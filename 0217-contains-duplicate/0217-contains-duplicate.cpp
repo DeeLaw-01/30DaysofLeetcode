28f8064a8981d83f87983eb20bc9eb9907ce0e2c
DeeLaw-01 <121474740+DeeLaw-01@users.noreply.github.com>
2024-03-17 15:13:59 +0500

    Time: 81 ms (89.46%), Space: 60.7 MB (91.74%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int size = nums.size();
+        sort(nums.begin(), nums.end());
+        for(size_t i =1; i < size; ++i){
+            if(nums[i] == nums[i-1]){
+                return true;
+            }
+        }
+        return false;
+    }
+};
