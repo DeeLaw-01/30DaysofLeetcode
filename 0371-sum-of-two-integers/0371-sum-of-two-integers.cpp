45c6cf8f8ca1673d87c6ac0956b32d5abaf62b9f
DeeLaw-01 <121474740+DeeLaw-01@users.noreply.github.com>
2024-03-18 17:45:59 +0500

    Time: 0 ms (100.00%), Space: 7.2 MB (22.25%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        int carry =0;
+        int car =0;
         while (b != 0) {
-            carry = a & b;
+            car = a & b;
             a = a ^ b;
-            b = (unsigned)carry << 1;
+            b = (unsigned)car << 1;
         }
         return a;
     }
