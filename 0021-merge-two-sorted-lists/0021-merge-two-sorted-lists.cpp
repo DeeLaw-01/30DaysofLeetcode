0e01184139833232a6f449d1356c5d262a2da0eb
DeeLaw-01 <121474740+DeeLaw-01@users.noreply.github.com>
2024-03-21 05:27:44 +0500

    Time: 3 ms (82.47%), Space: 18.4 MB (41.61%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };r
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == nullptr && l2 == nullptr){return nullptr;}
+        if(l1 == nullptr){return l2;}
+        if(l2== nullptr){return l1;}
+        int min = 0;
+        if(l1->val < l2->val){
+            min = l1->val;
+            l1 = l1->next;
+        }
+        else{
+            min = l2->val;
+            l2 = l2->next;
+        }
+        ListNode* sol = new ListNode(min);
+        ListNode* temp = sol;
+        while(l1 != nullptr && l2 != nullptr){
+            if(l1->val < l2->val){
+                temp->next = l1;
+                l1 = l1->next;
+            }
+            else{
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            temp = temp->next;
+        }
+        if(l1 == nullptr && l2 == nullptr){
+            return sol;
+        }
+        if(l1 == nullptr){
+            temp->next = l2;
+        }
+        else{
+            temp->next = l1;
+        }
+        return sol;
+    }
+
+        
+
+};
