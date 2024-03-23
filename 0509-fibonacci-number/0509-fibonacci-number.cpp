a192aebc116cbbee1673543315814a8ffc59d508
DeeLaw-01 <121474740+DeeLaw-01@users.noreply.github.com>
2024-03-23 05:39:16 +0500

    Time: 0 ms (100.00%), Space: 7.3 MB (12.58%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int fib(int n) {
         //Return 0 if n is 0
-        if(n==0){return n;}
+        if(n==0){return 0;}
         vector <int> fib(n+1);
         fib[0] =0; fib[1] =1;
         for(size_t i =2; i <=n; ++i){
