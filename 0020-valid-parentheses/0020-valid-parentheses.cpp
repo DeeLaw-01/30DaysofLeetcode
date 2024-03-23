4b56f94e48db2894d410896bdd4e9882e3c3f70b
DeeLaw-01 <121474740+DeeLaw-01@users.noreply.github.com>
2024-03-23 16:35:20 +0500

    Time: 0 ms (100.00%), Space: 7.6 MB (61.00%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,7 +2,7 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.length() %2 ==1){
+        if(s.length() %2 !=0){
             return false;
         }
         stack <char> st;
