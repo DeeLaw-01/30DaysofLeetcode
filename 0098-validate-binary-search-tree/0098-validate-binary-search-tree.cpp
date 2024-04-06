82bcb10f9bdb63e61d1a99867b76434069d28d7f
DeeLaw-01 <121474740+DeeLaw-01@users.noreply.github.com>
2024-04-06 20:24:02 +0500

    Time: 3 ms (97.94%), Space: 20.5 MB (7.38%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        vector<int> inorder;
+        inorderTraversal(root, inorder);
+        
+        // Check if the inorder traversal result is sorted
+        for (int i = 1; i < inorder.size(); ++i) {
+            if (inorder[i] <= inorder[i - 1]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+private:
+    void inorderTraversal(TreeNode* root, vector<int>& inorder) {
+        if (!root) return;
+        inorderTraversal(root->left, inorder);
+        inorder.push_back(root->val);
+        inorderTraversal(root->right, inorder);
+    }
+};
